7d766337ac3d27c6846941b6fbb844292ca1a8a8
brookejforrester@gmail.com <>
2022-11-02 01:16:34 +1100

    Made some changes, Added a plain enemy sprite that moves from left to right

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
-{
+{   
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +13,20 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // make enemy move left and right
+        transform.position = new Vector3(Mathf.PingPong(Time.time * 1.5f, 6), transform.position.y, transform.position.z);
+
+        // make enemy pause for a second before moving again
+        if (Time.time % 2 == 0)
+        {
+            // make enemy pause for a second before moving again in the direction it was moving in
+            transform.position = new Vector3(Mathf.PingPong(Time.time * 0, 6), transform.position.y, transform.position.z);
+
+        }
         
+        
+    
     }
+
 }
+
