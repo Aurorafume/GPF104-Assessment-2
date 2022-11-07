be78b663c5995134a0c0dc8869a64f164ddfa740
brookejforrester@gmail.com <>
2022-11-08 04:21:12 +1100

    Fixed the movement so the enemy sprite flips

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -4,29 +4,48 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {   
+    public int enemyHP = 3;
+    private bool isFacingRight = true;
+    private Vector3 startingPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startingPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        // make enemy move left and right
-        transform.position = new Vector3(Mathf.PingPong(Time.time * 1.5f, 6), transform.position.y, transform.position.z);
+    {   
 
-        // make enemy pause for a second before moving again
-        if (Time.time % 2 == 0)
-        {
-            // make enemy pause for a second before moving again in the direction it was moving in
-            transform.position = new Vector3(Mathf.PingPong(Time.time * 0, 6), transform.position.y, transform.position.z);
+        int distance = (int)Vector3.Distance(transform.position, startingPosition);
+        
+        transform.position = new Vector3(transform.position.x + (isFacingRight? -0.005f : +0.005f), transform.position.y, transform.position.z);
 
+        if (distance > 5)
+        {   
+            startingPosition = transform.position;
+            Flip();
         }
-        
-        
-    
+    }
+
+    // make enemy notice player and move towards player when player is in range
+    void playerSensor(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            transform.position = Vector3.MoveTowards(transform.position, other.transform.position, 1 * Time.deltaTime);
+            // make enemy deal 2 damage to player
+            other.gameObject.GetComponent<Heart_System>().life -= 2;
+            
+        }
+    } 
+
+    private void Flip()
+    {   
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
     }
 
 }
-
--- Assets/Scripts/Projectile_Player.cs
+++ Assets/Scripts/Projectile_Player.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Projectile_Player : MonoBehaviour
 {   
-    Rigidbody2D rb2d;
+    
 
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        
     }
 
     void Update()
-    {   // send bullet forward
-        rb2d.velocity = new Vector2(10, 0);
+    {   
+        
     }
 }
