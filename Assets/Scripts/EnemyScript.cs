4112baf45129d01cc6405575c1ff4dbd41779eb0
brookejforrester@gmail.com <>
2022-11-08 19:05:39 +1100

    Fixed stuff

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -9,7 +9,15 @@ public class EnemyScript : MonoBehaviour
     private Vector3 startingPosition;
     private bool playerDetected = false;
     public int playerSensorDistance = 5;
+    [field: SerializeField]
     private Collider2D playerSensor;
+
+    [field: SerializeField]
+    public bool PlayerInArea { get; private set; }
+    public Transform Player { get; private set; }
+
+    [SerializeField]
+    private string detectionTag = "Player";
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +28,6 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
-
         int distance = (int)Vector3.Distance(transform.position, startingPosition);
         
         transform.position = new Vector3(transform.position.x + (isFacingRight? -0.003f : +0.003f), transform.position.y, transform.position.z);
@@ -39,4 +46,22 @@ public class EnemyScript : MonoBehaviour
         localScale.x *= -1f;
         transform.localScale = localScale;
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+        {   
+            if (collision.CompareTag(detectionTag))
+            {
+                PlayerInArea = true;
+                Player = collision.gameObject.transform;
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.CompareTag(detectionTag))
+            {
+                PlayerInArea = false;
+                Player = null;
+            }
+        }
 }
